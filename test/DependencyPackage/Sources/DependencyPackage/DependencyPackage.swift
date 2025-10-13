450998bb639ebdddfacab0132c0a3d630d037ba5
leogdion <leogdion@brightdigit.com>
2025-10-13 11:20:24 -0400

    fixing build for DependencyPackage

--- test/DependencyPackage/Sources/DependencyPackage/DependencyPackage.swift
+++ test/DependencyPackage/Sources/DependencyPackage/DependencyPackage.swift
@@ -4,7 +4,7 @@ public struct DependencyPackage {
     public init() {}
 
     public func uniqueElements<T: Hashable>(from array: [T]) -> [T] {
-        return array.uniqued()
+      return .init(array.uniqued())
     }
 
     public func chunked<T>(_ array: [T], size: Int) -> [[T]] {
