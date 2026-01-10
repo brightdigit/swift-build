be1616862050389a91b8f325f34b00b87e1a8472
Leo Dion <leogdion@brightdigit.com>
2026-01-09 19:07:17 -0500

    undo change to MultiTargetPackage

--- test/MultiTargetPackage/Package.swift
+++ test/MultiTargetPackage/Package.swift
@@ -14,9 +14,6 @@ let package = Package(
             name: "Utils",
             targets: ["Utils"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-testing.git", from: "0.12.0"),
-    ],
     targets: [
         .target(
             name: "Core",
@@ -25,15 +22,9 @@ let package = Package(
             name: "Utils"),
         .testTarget(
             name: "CoreTests",
-            dependencies: [
-                "Core",
-                .product(name: "Testing", package: "swift-testing")
-            ]),
+            dependencies: ["Core"]),
         .testTarget(
             name: "UtilsTests",
-            dependencies: [
-                "Utils",
-                .product(name: "Testing", package: "swift-testing")
-            ]),
+            dependencies: ["Utils"]),
     ]
 ) 
