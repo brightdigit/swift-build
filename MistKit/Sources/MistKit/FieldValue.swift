98a215b2be7074120d35c0771295e7fb23f2992d
Leo Dion <leogdion@brightdigit.com>
2026-01-14 09:49:06 -0500

    git subrepo clone --branch=swift-build-1.5.0-beta.2 git@github.com:brightdigit/MistKit.git
    subrepo:
      subdir:   "MistKit"
      merged:   "7ae45ca"
    upstream:
      origin:   "git@github.com:brightdigit/MistKit.git"
      branch:   "swift-build-1.5.0-beta.2"
      commit:   "7ae45ca"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/Homebrew/brew"
      commit:   "1383417817"

---
+++ MistKit/Sources/MistKit/FieldValue.swift
@@ -0,0 +1,245 @@
+//
+//  FieldValue.swift
+//  MistKit
+//
+//  Created by Leo Dion.
+//  Copyright © 2026 BrightDigit.
+//
+//  Permission is hereby granted, free of charge, to any person
+//  obtaining a copy of this software and associated documentation
+//  files (the “Software”), to deal in the Software without
+//  restriction, including without limitation the rights to use,
+//  copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following
+//  conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//  OTHER DEALINGS IN THE SOFTWARE.
+//
+
+public import Foundation
+
+/// Represents a CloudKit field value as defined in the CloudKit Web Services API
+public enum FieldValue: Codable, Equatable, Sendable {
+  case string(String)
+  case int64(Int)
+  case double(Double)
+  case bytes(String)  // Base64-encoded string
+  case date(Date)  // Date/time value
+  case location(Location)
+  case reference(Reference)
+  case asset(Asset)
+  case list([FieldValue])
+
+  /// Conversion factor from seconds to milliseconds for CloudKit timestamps
+  private static let millisecondsPerSecond: Double = 1_000
+
+  /// Location dictionary as defined in CloudKit Web Services
+  public struct Location: Codable, Equatable, Sendable {
+    /// The latitude coordinate
+    public let latitude: Double
+    /// The longitude coordinate
+    public let longitude: Double
+    /// The horizontal accuracy in meters
+    public let horizontalAccuracy: Double?
+    /// The vertical accuracy in meters
+    public let verticalAccuracy: Double?
+    /// The altitude in meters
+    public let altitude: Double?
+    /// The speed in meters per second
+    public let speed: Double?
+    /// The course in degrees
+    public let course: Double?
+    /// The timestamp when location was recorded
+    public let timestamp: Date?
+
+    /// Initialize a location value
+    public init(
+      latitude: Double,
+      longitude: Double,
+      horizontalAccuracy: Double? = nil,
+      verticalAccuracy: Double? = nil,
+      altitude: Double? = nil,
+      speed: Double? = nil,
+      course: Double? = nil,
+      timestamp: Date? = nil
+    ) {
+      self.latitude = latitude
+      self.longitude = longitude
+      self.horizontalAccuracy = horizontalAccuracy
+      self.verticalAccuracy = verticalAccuracy
+      self.altitude = altitude
+      self.speed = speed
+      self.course = course
+      self.timestamp = timestamp
+    }
+  }
+
+  /// Reference dictionary as defined in CloudKit Web Services
+  public struct Reference: Codable, Equatable, Sendable {
+    /// Reference action types supported by CloudKit
+    public enum Action: String, Codable, Sendable {
+      case deleteSelf = "DELETE_SELF"
+      case none = "NONE"
+    }
+
+    /// The record name being referenced
+    public let recordName: String
+    /// The action to take (DELETE_SELF, NONE, or nil)
+    public let action: Action?
+
+    /// Initialize a reference value
+    public init(recordName: String, action: Action? = nil) {
+      self.recordName = recordName
+      self.action = action
+    }
+  }
+
+  /// Asset dictionary as defined in CloudKit Web Services
+  public struct Asset: Codable, Equatable, Sendable {
+    /// The file checksum
+    public let fileChecksum: String?
+    /// The file size in bytes
+    public let size: Int64?
+    /// The reference checksum
+    public let referenceChecksum: String?
+    /// The wrapping key for encryption
+    public let wrappingKey: String?
+    /// The upload receipt
+    public let receipt: String?
+    /// The download URL
+    public let downloadURL: String?
+
+    /// Initialize an asset value
+    public init(
+      fileChecksum: String? = nil,
+      size: Int64? = nil,
+      referenceChecksum: String? = nil,
+      wrappingKey: String? = nil,
+      receipt: String? = nil,
+      downloadURL: String? = nil
+    ) {
+      self.fileChecksum = fileChecksum
+      self.size = size
+      self.referenceChecksum = referenceChecksum
+      self.wrappingKey = wrappingKey
+      self.receipt = receipt
+      self.downloadURL = downloadURL
+    }
+  }
+
+  // MARK: - Codable
+  /// Initialize field value from decoder
+  public init(from decoder: any Decoder) throws {
+    let container = try decoder.singleValueContainer()
+
+    if let value = try Self.decodeBasicTypes(from: container) {
+      self = value
+      return
+    }
+
+    if let value = try Self.decodeComplexTypes(from: container) {
+      self = value
+      return
+    }
+
+    throw DecodingError.dataCorruptedError(
+      in: container,
+      debugDescription: "Unable to decode FieldValue"
+    )
+  }
+
+  /// Decode basic field value types (string, int64, double)
+  private static func decodeBasicTypes(from container: any SingleValueDecodingContainer) throws
+    -> FieldValue?
+  {
+    if let value = try? container.decode(String.self) {
+      return .string(value)
+    }
+    if let value = try? container.decode(Int.self) {
+      return .int64(value)
+    }
+    if let value = try? container.decode(Double.self) {
+      return .double(value)
+    }
+    return nil
+  }
+
+  /// Decode complex field value types (list, location, reference, asset, date)
+  private static func decodeComplexTypes(from container: any SingleValueDecodingContainer) throws
+    -> FieldValue?
+  {
+    if let value = try? container.decode([FieldValue].self) {
+      return .list(value)
+    }
+    if let value = try? container.decode(Location.self) {
+      return .location(value)
+    }
+    if let value = try? container.decode(Reference.self) {
+      return .reference(value)
+    }
+    if let value = try? container.decode(Asset.self) {
+      return .asset(value)
+    }
+    // Try to decode as date (milliseconds since epoch)
+    if let value = try? container.decode(Double.self) {
+      return .date(Date(timeIntervalSince1970: value / Self.millisecondsPerSecond))
+    }
+    return nil
+  }
+
+  /// Encode field value to encoder
+  public func encode(to encoder: any Encoder) throws {
+    var container = encoder.singleValueContainer()
+    try encodeValue(to: &container)
+  }
+
+  // swiftlint:disable:next cyclomatic_complexity
+  private func encodeValue(to container: inout any SingleValueEncodingContainer) throws {
+    switch self {
+    case .string(let val), .bytes(let val):
+      try container.encode(val)
+    case .int64(let val):
+      try container.encode(val)
+    case .double(let val):
+      try container.encode(val)
+    case .date(let val):
+      try container.encode(val.timeIntervalSince1970 * Self.millisecondsPerSecond)
+    case .location(let val):
+      try container.encode(val)
+    case .reference(let val):
+      try container.encode(val)
+    case .asset(let val):
+      try container.encode(val)
+    case .list(let val):
+      try container.encode(val)
+    }
+  }
+}
+
+// MARK: - Helper Methods
+
+extension FieldValue {
+  /// Create an int64 FieldValue from a Bool
+  ///
+  /// CloudKit represents booleans as INT64 (0/1) on the wire.
+  /// Creates a FieldValue from a Swift Bool value.
+  ///
+  /// This initializer converts Swift Bool to the appropriate int64 representation
+  /// used by CloudKit (1 for true, 0 for false).
+  ///
+  /// - Parameter booleanValue: The boolean value to convert
+  public init(booleanValue: Bool) {
+    self = .int64(booleanValue ? 1 : 0)
+  }
+}
